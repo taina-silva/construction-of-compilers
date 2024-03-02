f1dc4a21177e49236a8dbb02433cd7d135173f0c
Bruno Ferreira <49572026+brunoftom@users.noreply.github.com>
2024-03-02 16:09:12 -0300

    tem que terminar alguns casos (poucos) and revisar

--- src/analyzers/lexical_analyzer.py
+++ src/analyzers/lexical_analyzer.py
@@ -2,8 +2,76 @@ import sys
 
 from src.utils.buffer import Buffer
 from src.exceptions.inappropriate_character_exception import InappropriateCharacterException
+from src.utils.token import token as tk
+from src.analyzers import symbol_table
 
 buffer = Buffer(file=sys.argv[1])
+
+class LexerError(Exception):
+    """Errors on Lexer"""
+
+    position: tuple[int, int]
+
+    def __init__(self, position: tuple[int, int]) -> None:
+        self.position = position
+
+
+def operadores_relacionais_token(op_code: int) -> tk.Token:
+    return tk.Token("operadores_relacionais", op_code)
+
+
+def common_symbol(symbol: str) -> tk.Token:
+    return tk.Token(symbol, None)
+
+
+def set_id(identifier: str) -> tk.Token:
+    position = symbol_table.lookup(identifier)
+    if position is None:
+        position = symbol_table.append(identifier, "ID", None, None)
+    return tk.Token("ID", position)
+
+
+def set_char(character: str) -> tk.Token:
+    position = symbol_table.lookup(character)
+    if position is None:
+        position = symbol_table.append(
+            lexemn=character, token_type="CONST_CHAR", value=character, data_type="char"
+        )
+    return tk.Token("CONST_CHAR", position)
+
+
+def set_int(integer: str) -> tk.Token:
+    position = symbol_table.lookup(integer)
+    if position is None:
+        position = symbol_table.append(
+            lexemn=integer, token_type="CONST_NUM", value=integer, data_type="int"
+        )
+    return tk.Token("CONST_NUM", position)
+
+
+def set_frac(fractional_nbr: str) -> tk.Token:
+    position = symbol_table.lookup(fractional_nbr)
+    if position is None:
+        position = symbol_table.append(
+            lexemn=fractional_nbr,
+            token_type="CONST_NUM",
+            value=fractional_nbr,
+            data_type="float",
+        )
+    return tk.Token("CONST_NUM", position)
+
+
+def set_exp(exp: str) -> tk.Token:
+    position = symbol_table.lookup(exp)
+    if position is None:
+        position = symbol_table.append(
+            lexemn=exp,
+            token_type="CONST_NUM",
+            value=exp,
+            data_type="float",
+        )
+    return tk.Token("CONST_NUM", position)
+
 
 def get_token():
     state = 1  # estado inicial 
@@ -59,14 +127,16 @@ def get_token():
                 else:
                     state = 3
             case 3:
-                
                 return
             case 4:
-                return
+                location = buffer.scan_point.location
+                return common_symbol(buffer.sync()), location
             case 5:
-                return
+                location = buffer.scan_point.location
+                return common_symbol(buffer.sync()), location
             case 6:
-                return
+                location = buffer.scan_point.location
+                return common_symbol(buffer.sync()), location
             case 7:
                 char = buffer.next_char
 
@@ -75,7 +145,8 @@ def get_token():
                 else:
                     state = 7 
             case 8:
-                return
+                buffer.sync()
+                state = 1 #reinicia a função para ignorar comentários
             case 9:
                 char = buffer.next_char
 
@@ -84,7 +155,9 @@ def get_token():
                 else:
                     return InappropriateCharacterException(char)
             case 10:
-                return
+                location = buffer.scan_point.location
+                buffer.sync()
+                return operadores_relacionais_token(tk.operadores_relacionais_EQ), location
             case 11:
                 char = buffer.next_char
 
@@ -97,18 +170,23 @@ def get_token():
                 else:
                     state = 21
             case 12:
-                return
+                location = buffer.scan_point.location
+                buffer.sync()
+                return operadores_relacionais_token(tk.operadores_relacionais_LE), location
             case 13:
-                return
+                location = buffer.scan_point.location
+                buffer.sync()
+                return operadores_relacionais_token(tk.operadores_relacionais_NE), location
             case 14:
-                char - buffer.next_char
+                char = buffer.next_char
 
                 if char == '-':
                     state = 15
                 else:
                     return InappropriateCharacterException(char)
             case 15:
-                return
+                location = buffer.scan_point.location
+                return common_symbol(buffer.sync()), location
             case 16:
                 char = buffer.next_char
 
@@ -117,13 +195,19 @@ def get_token():
                 else:
                     state = 18
             case 17:
-                return
+                location = buffer.scan_point.location
+                buffer.sync()
+                return operadores_relacionais_token(tk.operadores_relacionais_GE), location
             case 18:
-                return
+                location = buffer.scan_point.location
+                buffer.sync(handle_lookahead = True)
+                return operadores_relacionais_token(tk.operadores_relacionais_GT), location
             case 19:
-                return
+                location = buffer.scan_point.location
+                return common_symbol(buffer.sync()), location
             case 20:
-                return
+                location = buffer.scan_point.location
+                return common_symbol(buffer.sync()), location
             case 21:
                 char = buffer.next_char
 
@@ -132,9 +216,12 @@ def get_token():
                 else:
                     state = 23
             case 22:
-                return
+                location = buffer.scan_point.location
+                return common_symbol(buffer.sync()), location
             case 23:
-                return
+                location = buffer.scan_point.location
+                buffer.sync(handle_lookahead=True)
+                return common_symbol(buffer.sync()), location
             case 24:
                 char = buffer.next_char
 
@@ -143,11 +230,15 @@ def get_token():
                 else:
                     state = 26
             case 25:
-                return
+                location = buffer.scan_point.location
+                return common_symbol(buffer.sync()), location
             case 26:
-                return
+                location = buffer.scan_point.location
+                buffer.sync(handle_lookahead=True)
+                return common_symbol(buffer.sync()), location
             case 27:
-                return
+                location = buffer.scan_point.location
+                return common_symbol(buffer.sync()), location
             case 28:
                 char = buffer.next_char  
 
@@ -156,6 +247,8 @@ def get_token():
                 else:
                     return InappropriateCharacterException(char)
             case 29:
+               char = buffer.next_char 
+
                if char == "'":
                    state = 30
                else:
@@ -165,7 +258,7 @@ def get_token():
             case 31:
                 char = buffer.next_char
 
-                if char == char.isdigit():
+                if char.isdigit():
                     state = 31
                 elif char == '.':
                     state = 33
@@ -194,27 +287,36 @@ def get_token():
             case 35:
                 return
             case 36:
-                return
+                char = buffer.next_char
+
+                if char == '+-':
+                    state = 37
+                elif char == char.isdigit():
+                    state = 38
+                else:
+                    return InappropriateCharacterException(char)
             case 37:
                 char = buffer.next_char
 
-                if char == char.isdigit():
+                if char.isdigit():
                     state = 38
                 else:
                     return InappropriateCharacterException(char)
             case 38:
                 char = buffer.next_char
 
-                if char == char.isdigit():
+                if char.isdigit():
                     state = 38
                 else:
                     state = 39
             case 39:
                 return
             case 40:
-                return
+                location = buffer.scan_point.location
+                return common_symbol(buffer.sync()), location
             case 41:
-                return
+                location = buffer.scan_point.location
+                return common_symbol(buffer.sync()), location
             case 42:
                 char = buffer.next_char
 
@@ -223,6 +325,7 @@ def get_token():
                 else:
                     state = 43
             case 43:
-                return
+                buffer.sync(handle_lookahead=True)
+                state = 1  
             case _:
-                return
+                break; #erro
